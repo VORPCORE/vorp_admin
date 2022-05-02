15dc82fbc00fd8875aa67203c933b57ca83759e2
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-02 22:33:28 +0100

    player list info

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,6 @@
+----------------------------------------------------------------------------------------------------
+------------------------------------- SERVER ------------------------------------------------------
+
 local VorpCore = {}
 
 TriggerEvent("getCore", function(core)
@@ -14,4 +17,49 @@ AddEventHandler("vorp_admin:GetGroup", function()
         TriggerClientEvent("vorp_admin:OpenMenu", _source) --open menu
 
     end
+end)
+
+--get players info list
+RegisterServerEvent('vorp_admin:GetPlayers')
+AddEventHandler('vorp_admin:GetPlayers', function() --TODO get players info char name and last name steam license etc
+    local _source = source
+    local players = GetPlayers()
+    local User = VorpCore.getUser(_source)
+    local group = User.getGroup -- user table
+    local Character = User.getUsedCharacter --get player info
+    local playername = Character.firstname .. ' ' .. Character.lastname --player char name
+    local job = Character.job --player job
+    local identifier = Character.identifier --player steam
+    local PlayerMoney = Character.money --money
+    local PlayerGold = Character.gold --gold
+    local JobGrade = Character.jobGrade --jobgrade
+
+    local data = {}
+
+    for _, player in ipairs(players) do
+        local playerPed = GetPlayerPed(player)
+
+        if DoesEntityExist(playerPed) then
+
+            local coords = GetEntityCoords(playerPed)
+
+            data[tostring(player)] = {
+                serverId = player,
+                networkId = NetworkGetNetworkIdFromEntity(playerPed),
+                x = coords.x,
+                y = coords.y,
+                z = coords.z,
+                name = GetPlayerName(player),
+                Group = group,
+                PlayerName = playername,
+                Job = job,
+                SteamId = identifier,
+                ped = playerPed,
+                Money = PlayerMoney,
+                Gold = PlayerGold,
+                Grade = JobGrade
+            }
+        end
+    end
+    TriggerClientEvent("vorp_admin:SendPlayers", _source, data)
 end)
