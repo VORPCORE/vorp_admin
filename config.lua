9bc45264dec2e290cf2e4c8ab44fc3d9925c0780
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-03 23:31:11 +0100

    updated

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
 Config = {
-
+    --add your language
     defaultlang = "en_lang",
-
-
+    Key = 0x3C3DD371, --PGDOWN
+    CanOpenMenuWhenDead = true -- if true any staff can open menu when dead, !WARNING! staff can abuse this to get revived ETC
 
 
 }
