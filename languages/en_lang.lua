fa528f6120e61d04a93d4b517a5438aa67d1d269
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-04 19:26:56 +0100

    update lang

--- languages/en_lang.lua
+++ languages/en_lang.lua
@@ -4,5 +4,34 @@
 
 Locales["en_lang"] = {
 
+    MenuTitle = "VORP ADMIN",
+    MenuSubTitle = "Main menu",
 
+    -----------------------------
+    -- submenu player list Label
+    MenuSubtitle2 = "Players online",
+    kick_p = "Kick",
+    freeze_p = "Freeze",
+    unfreeze_p = "Unfreeze",
+    ban_p = "Ban",
+    spectate_p = "Sectate",
+    revive_p = "Revive",
+    respawn_p = "Respawn",
+    heal_p = "Heal",
+    goto_p = "Go To",
+    bring_p = "Bring",
+    warn_p = "Warn",
+    -- the description of the above ^^
+    kick_desc = "Kick ",
+    freeze_desc = "Freeze ",
+    unfreeze_desc = "Unfreeze ",
+    ban_desc = "Ban ",
+    spectate_desc = "Spectate",
+    revive_desc = "Revive",
+    respawn_desc = "Respawn ",
+    heal_desc = "Heal ",
+    goto_desc = "Go to ",
+    bring_desc = "Bring",
+    warn_desc = "Warn",
+    -----------------------------
 }
