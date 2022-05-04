449e7b5d297a22d193956b84be202e3abb1043bb
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-04 19:28:21 +0100

    Add files via upload

--- client/users.lua
+++ client/users.lua
@@ -1,5 +1,32 @@
---------------------------------------------------------------------
---------------------------- USERS MENU -----------------------------
+------------------------------------------ USERS MENU ------------------------------------------------
+VORPUsers = {}
 
-VORPUsers.User = {}
+VORPUsers.OpenUsersMenu = function()
+    MenuData.CloseAll()
+    local elements = {
+        { label = "Report", value = 'report', desc = "" },
+        { label = "request staff", value = 'requestStaff', desc = "" },
+        { label = "create ticket", value = 'ticket', desc = "" },
+        { label = "show My Info", value = 'showinfo', desc = "" },
+    }
 
+    MenuData.Open('default', GetCurrentResourceName(), 'menuapi',
+        {
+            title    = _U("MenuTitle"),
+            subtext  = _U("MenuSubTitle"),
+            align    = 'top-left',
+            elements = elements,
+        },
+        function(data, menu)
+            if data.current == "backup" then
+                _G[data.trigger]()
+            end
+
+
+        end,
+
+        function(data, menu)
+        menu.close()
+
+    end)
+end
