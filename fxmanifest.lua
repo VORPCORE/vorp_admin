c597a4dcc7834ef0146fe2bd5b03bfaa08cdc1f4
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-06 20:05:08 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'outsider'
 description 'VORP admin menu'
-version '1.0.0'
+version '1.0'
 
 client_scripts {
     'client/*.lua',
@@ -17,5 +17,11 @@ shared_script {
 }
 
 server_scripts {
-    'server/*.lua'
+    'server/*.lua',
+    'versioncheck.lua',
 }
+
+
+name '^2FiveM Vehicle Loader'
+github 'https://github.com/VORPCORE/vorp_admin'
+fivem_checker 'yes'
