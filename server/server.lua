72e9e95814f68cd37af3a0c4008518da66345f8e
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-07 00:01:47 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -71,3 +71,25 @@ AddEventHandler('vorp_admin:GetPlayers', function() --TODO get players info char
     end
     TriggerClientEvent("vorp_admin:SendPlayers", _source, data)
 end)
+
+Citizen.CreateThread(function()
+    updatePath = "/VORPCORE/vorp_admin" -- your git user/repo path
+    resourceName = "FiveM-Resource-Version-Check-Thing (" .. GetCurrentResourceName() .. ")" -- the resource name
+    print(updatePath)
+    function checkVersion(err, responseText, headers)
+        curVersion = LoadResourceFile(GetCurrentResourceName(), "version.json") -- make sure the "version" file actually exists in your resource root!
+        print(curVersion) -- works
+        print(responseText)
+        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+            print("\n###############################")
+            print("\n" .. resourceName .. " is outdated, should be:\n" .. responseText .. "is:\n" .. curVersion .. "\nplease update it from https://github.com/VORPCORE/vorp_admin")
+            print("\n###############################")
+        elseif tonumber(curVersion) > tonumber(responseText) then
+            print("You somehow skipped a few versions of " .. resourceName .. " or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+        else
+            print("\n" .. resourceName .. " is up to date, have fun!")
+        end
+    end
+
+    PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/master/version", checkVersion, "GET")
+end)
