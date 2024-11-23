d0832204c54a6989dd65351e0ef169bda08ba920
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-11-23 21:42:08 +0000

    Create logs.lua

---
+++ logs.lua
@@ -0,0 +1,64 @@
+Logs = {
+    webhookColor = 16711680,                            -- EMBED COLOR RED
+    name         = "VORP",                              -- NAME OF EMBED
+    logo         = "https://via.placeholder.com/30x30", -- HEAD LOGO
+    footerLogo   = "https://via.placeholder.com/30x30", -- FOOTER LOGO
+    avatar       = "https://via.placeholder.com/30x30", -- AVATAR LOGO
+
+    ReportLogs   = {
+        Reports = "",      -- for reports
+        RequestStaff = "", -- for request staff
+        BugReport = "",    -- for bug report
+        RulesBroken = "",  -- for rules broken report
+        Cheating = ""      -- for cheating report
+    },
+    BoosterLogs  = {
+        NoClip = "",
+        InfiniteAmmo = "",
+        GoldenCores = "",
+        GodMode = "",
+        SelfHeal = "",
+        SelfRevive = "",
+        SelfSpawnHorse = "",
+        SelfSpawnWagon = "",
+    },
+    AdminLogs    = {
+
+        -- Simple actions
+        Freezed = "",
+        Bring = "",
+        Goto = "",
+        Revive = "",
+        Heal = "",
+        Spectate = "",
+
+        -- Advanced actions
+        Respawn = "",
+        Kick = "",
+        Ban = "",
+        Unban = "",
+        Whitelist = "",
+        Unwhitelist = "",
+        Setgroup = "",
+        Setjob = "",
+        Announce = ""
+
+    },
+    TeleportLogs = {
+        Tpm = "",
+        Tptocoords = "",
+        Tptoplayer = "",
+        Bringplayer = "",
+    },
+    DatabaseLogs = {
+        Giveitem = "",
+        Giveweapon = "",
+        Givecurrency = "",
+        Givehorse = "",
+        Givewagon = "",
+        Clearmoney = "",
+        Cleargold = "",
+        Clearitems = "",
+        Clearweapons = "",
+    }
+}
